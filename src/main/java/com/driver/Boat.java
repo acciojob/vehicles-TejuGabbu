1711e81dc72f5586544a605425a2f85992b1515f
TejuGabbu <tskulkarni7875900168@gmail.com>
2023-10-15 17:54:30 +0530

    third commit

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -2,8 +2,8 @@ package com.driver;
 
 public class Boat implements WaterVehicle
 {
-    String name;
-    int capacity;
+    private String name;
+    private int capacity;
 
     public Boat(String name, int capacity)
     {
